c5d683cd17150cee75fabbe015506b30b30a1d37
Pankaj Singh <singh.pankaj@live.com>
2018-08-18 21:43:37 +0530

    Create homework 2.2.js

---
+++ Week02/09_Homework/homework 2.2.js
@@ -0,0 +1,18 @@
+/*Using the video.movieDetails collection, which of the queries below would produce output documents that resemble the following. 
+Check all that apply.
+
+NOTE: We are not asking you to consider specifically which documents would be output from the queries below, 
+but rather what fields the output documents would contain.
+
+{ "title" : "P.S. I Love You" }
+{ "title" : "Love Actually" }
+{ "title" : "Shakespeare in Love" }
+*/
+```js
+
+db.moviesDetails.find({},{title:1, _id:0})
+
+db.moviesDetails.find({},{title:1})
+
+db.moviesDetails.find({year:1964},{title:1, _id:0})
+
