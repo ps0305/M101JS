bbe18c208158ef0b64d29cc25fa34c88fa25fabd
Pankaj Singh <singh.pankaj@live.com>
2018-09-13 16:47:10 +0530

    Add files via upload

---
+++ Week06/Aggregation_framework.js
@@ -0,0 +1,6 @@
+//the aggregation pipeline
+
+
+1- the task a pipeline stage preforms is tunable.
+2-Aggregation pipelines are similar to Linux shell pipelines.
+3-Pipelines use a MongoDB collection as input.
