b281721cf1b5b756debb64668ac45f833e94ce42
Pankaj Singh <singh.pankaj@live.com>
2018-09-19 15:07:10 +0530

    Update write-concern.js

--- Week07-Application-engineering/write-concern.js
+++ Week07-Application-engineering/write-concern.js
@@ -1,2 +1,4 @@
 //Provided you assume that the disk is persistent, what are the w and j settings required to guarantee that an insert or update has been written all the way to disk.
 
+```js
+w=1,j=1
